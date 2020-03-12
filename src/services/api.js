10e58802b3770f30c9a7c365937807a4a2c005a3
Matheus Oliveira <mattoliveirabsb@gmail.com>
2020-03-12 16:01:50 -0600

    updated api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333/",
+  baseURL: "https://codechallenge-vutiliti-backend.herokuapp.com/",
   headers: {
     "content-type": "application/json",
     token: localStorage.token,
